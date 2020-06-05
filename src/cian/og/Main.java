897ffc8e48975bff8e7ffecc494ec4c3d69339e0
Cian O'Gralaigh <ihatecian@gmail.com>
2020-06-05 12:28:39 +0100

    Add user input

---
+++ src/cian/og/Main.java
@@ -0,0 +1,16 @@
+package cian.og;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter your name");
+        String name = scanner.nextLine();
+        System.out.println("Hello " + name);
+
+	    scanner.close();
+    }
+}
